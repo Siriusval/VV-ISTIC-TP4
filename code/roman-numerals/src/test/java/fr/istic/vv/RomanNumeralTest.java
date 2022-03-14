a3aec091a095020cf0965b1cdf7ee62b546b8fa2
Valentin Hulot <valentin.hulot@etudiant.univ-rennes1.fr>
2022-03-14 17:22:58 +0100

    jqwik test for nubers ending with 'I'

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -2,8 +2,20 @@ package fr.istic.vv;
 import net.jqwik.api.*;
 
 public class RomanNumeralTest {
+
     @Property
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
+    }
+
+    //Ending with 1 - I
+    @Property
+    boolean every_number_endingWith1_end_with_I(@ForAll("endingWith1") int i) {
+        return RomanNumeraUtils.toRomanNumeral(i).endsWith("I");
+    }
+
+    @Provide
+    Arbitrary<Integer> endingWith1() {
+        return Arbitraries.integers().between(1, 101).filter(i -> i % 10 == 1);
     }
 }
